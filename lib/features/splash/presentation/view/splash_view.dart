f2e0750c0f7bc1680212cc32a89e048488ae2b51
Michael Kalango <michaelkalango@gmail.com>
2020-07-25 18:19:09 +0100

    Feature Layer - Splash

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get_weather/core/utils/screen_util.dart';
+import 'package:stacked/stacked.dart';
+
+import '../viewmodel/splash_viewmodel.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<SplashViewModel>.reactive(
+        builder: (context, model, child) {
+          return Scaffold(
+            body: Container(
+              height: ScreenUtil(context).setHeight(),
+              width: ScreenUtil(context).setWidth(),
+              child: Center(
+                child: Text('SplashS'),
+              ),
+            ),
+          );
+        },
+        viewModelBuilder: () => SplashViewModel());
+  }
+}
---
+++ lib/features/splash/presentation/viewmodel/splash_viewmodel.dart
@@ -0,0 +1,6 @@
+import 'package:stacked/stacked.dart';
+
+class SplashViewModel extends BaseViewModel {
+  String _text = 'Splash Screen';
+  String get text => _text;
+}

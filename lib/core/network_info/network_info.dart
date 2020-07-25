a2fd12880ab20fd129cb9cfc2cee4f9b449fd840
Michael Kalango <michaelkalango@gmail.com>
2020-07-25 14:42:21 +0100

    Core Layer - Network Info

---
+++ lib/core/network_info/network_info.dart
@@ -0,0 +1,13 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:meta/meta.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final DataConnectionChecker connectionChecker;
+  NetworkInfoImpl({@required this.connectionChecker});
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+}

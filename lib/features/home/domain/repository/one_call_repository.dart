f56588198754ec9917b91ab8899868af99445982
Michael Kalango <michaelkalango@gmail.com>
2020-07-26 03:41:36 +0100

    Feature Layer - Home || Domain Layer - Repository update

--- lib/features/home/domain/repository/one_call_repository.dart
+++ lib/features/home/domain/repository/one_call_repository.dart
@@ -1,5 +1,8 @@
-import 'package:equatable/equatable.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../entities/open_weather_one_call.dart';
 
 abstract class OneCallRepository extends Equatable {
   Future<Either<Failure, OpenWeatherOneCall>> getOneCall();

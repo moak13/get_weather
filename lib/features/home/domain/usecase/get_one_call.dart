6dc29c878882437da68c9efc5e05810201b45a08
Michael Kalango <michaelkalango@gmail.com>
2020-07-26 03:48:25 +0100

    Feature Layer - Home || Domain Layer - Usecase

---
+++ lib/features/home/domain/usecase/get_one_call.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/usecase/usecase.dart';
+import '../entities/open_weather_one_call.dart';
+import '../repository/one_call_repository.dart';
+
+class GetOneCall implements UseCase<OpenWeatherOneCall, NoParams> {
+  final OneCallRepository repository;
+  GetOneCall({@required this.repository});
+  @override
+  Future<Either<Failure, OpenWeatherOneCall>> call(NoParams params) {
+    return repository.getOneCall();
+  }
+}
+
+class NoParams {}

5fd4877b4ced64783202cf365ef9c5e207a7b6d7
Michael Kalango <michaelkalango@gmail.com>
2020-07-26 03:53:47 +0100

    Core Layer - Errors update

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 // This class handles failures that tends to occur during application process
 abstract class Failure extends Equatable {
@@ -7,12 +8,16 @@ abstract class Failure extends Equatable {
 
 // Handles all server error messages
 class ServerError extends Failure {
+  final String message;
+  ServerError({@required this.message});
   @override
-  List<Object> get props => throw UnimplementedError();
+  List<Object> get props => [message];
 }
 
 // Handles all cache error messages
 class CacheError extends Failure {
+  final String message;
+  CacheError({@required this.message});
   @override
-  List<Object> get props => throw UnimplementedError();
+  List<Object> get props => [message];
 }

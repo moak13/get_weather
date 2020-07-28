67736c15dd4491e29cdf3b35f4110821eb3f5f57
Michael Kalango <michaelkalango@gmail.com>
2020-07-28 07:55:12 +0100

    Feature Layer - Search || Domain Layer - Repository

---
+++ lib/features/search/domain/repository/loation_weather_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../entities/open_weather_location.dart';
+
+abstract class LocationWeatherRepository extends Equatable {
+  Future<Either<Failure, OpenWeatherLocation>> getLocationWeather(
+      {@required String cityNmae});
+}

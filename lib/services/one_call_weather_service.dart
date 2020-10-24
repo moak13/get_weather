d816b1cccc80723f4da11afc576d747d6198f716
Michael Kalango <michaelkalango@gmail.com>
2020-10-24 16:29:00 +0100

    one call weather service

---
+++ lib/services/one_call_weather_service.dart
@@ -0,0 +1,40 @@
+import '../core/errors/exceptions.dart';
+import '../core/network/network_info.dart';
+import '../core/utils/result.dart';
+import '../data_source/local/one_call_weather_local_data_source.dart';
+import '../data_source/remote/one_call_weather_remote_data_source.dart';
+import '../locator.dart';
+import '../models/one_call_weather_model.dart';
+
+abstract class OneCallWeatherService {
+  Future<Result<OneCallWeatherModel>> performOneCallWeather(
+      {double lon, double lat});
+}
+
+class OneCallWeatherServiceImpl implements OneCallWeatherService {
+  final _networkInfo = locator<NetworkInfo>();
+  final _remoteData = locator<OneCallWeatherRemoteDataSource>();
+  final _localData = locator<OneCallWeatherLocalDataSource>();
+  @override
+  Future<Result<OneCallWeatherModel>> performOneCallWeather(
+      {double lon, double lat}) async {
+    bool isConnected = await _networkInfo.isConnected;
+    if (isConnected) {
+      try {
+        final response =
+            await _remoteData.fetchOneCallWeather(lat: lat, lon: lon);
+        _localData.saveResponse(oneCallWeatherModel: response);
+        return Result(success: response);
+      } on ServerException catch (err) {
+        return Result(error: Error(message: err.message));
+      }
+    } else {
+      try {
+        final response = await _localData.getResponse();
+        return Result(success: response);
+      } on CacheException {
+        return Result(error: Error(message: 'No Internet Connection'));
+      }
+    }
+  }
+}
